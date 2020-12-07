f13c49ffb93f6d4bbe04c4e0ca164343df1adec3
Tristan Peng <pengt@catlin.edu>
2020-12-07 11:56:34 -0800

    Fix warnings

---
+++ frontend/src/components/workstation/FileInfo.js
@@ -0,0 +1,11 @@
+import Window from './Window';
+
+const FileInfo = ({ anchor, filename }) => {
+  return (
+    <Window anchor={ anchor } title={ `File Info for ${ filename }` }>
+
+    </Window>
+  );
+};
+
+export default FileInfo;
