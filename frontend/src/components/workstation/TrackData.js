93e9332b08b94c0c1ce27122f7318136da5042b3
Tristan Peng <pengt@catlin.edu>
2020-12-16 19:06:11 -0800

    Add checks to graph

--- frontend/src/components/workstation/TrackData.js
+++ frontend/src/components/workstation/TrackData.js
@@ -1,14 +1,15 @@
 import { useState, useEffect, createRef } from 'react';
 import * as d3 from 'd3';
+import halfmoon from 'halfmoon';
 
 const TrackData = ({ file, column }) => {
   const svg = createRef();
   const xAxis = createRef();
   const yAxis = createRef();
 
-  const [state, setState] = useState({ line: '' });
+  const [state, setState] = useState({ graph: '' });
 
-  const data = file.map((row, i) => ([i, parseFloat(row[column])]));
+  const data = file.map((row, i) => ([i, isNaN(parseFloat(row[column])) ? row[column] : parseFloat(row[column])]));
 
   useEffect(() => {
     const width = getComputedStyle(svg.current).width;
@@ -21,25 +22,29 @@ const TrackData = ({ file, column }) => {
       .domain(d3.extent(data, datum => datum[1]))
       .range([parseFloat(height.slice(0, height.length - 2)) - 20, 0]);
 
-    const line = d3.line()
-      .x(data => x(data[0]))
-      .y(data => y(data[1]));
+    if (data.every(datum => !isNaN(datum[1]))) {
+      const line = d3.line()
+        .x(data => x(data[0]))
+        .y(data => y(data[1]));
 
-    const axes = [
-      d3.axisBottom()
-        .scale(x)
-        .ticks(0),
-      d3.axisLeft()
-        .scale(y)
-    ];
+      setState({ ...state, graph: line(data) });
 
-    d3.select(xAxis.current).call(axes[0]);
-    d3.select(yAxis.current).call(axes[1]);
+      const axes = [
+        d3.axisBottom()
+          .scale(x)
+          .ticks(0),
+        d3.axisLeft()
+          .scale(y)
+      ];
+
+      d3.select(xAxis.current).call(axes[0]);
+      d3.select(yAxis.current).call(axes[1]);
+    } else {
+      // TODO: Implement histogram
+    }
 
     xAxis.current.style.transform = `translateY(calc(${ height } - 20px))`;
     yAxis.current.style.transform = 'translateX(30px)';
-
-    setState({ ...state, line: line(data) });
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
@@ -48,7 +53,8 @@ const TrackData = ({ file, column }) => {
         <g className="x" ref={ xAxis } />
         <g className="y" ref={ yAxis } />
         <g className="line">
-          <path d={ state.line } fill="none" stroke="black" />
+          {/* TODO: Make responsive */}
+          <path d={ state.graph } fill="none" stroke={ halfmoon.darkModeOn ? 'white' : 'black' } />
         </g>
       </g>
     </svg>
