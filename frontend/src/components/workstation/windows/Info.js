c2f30be5dda6a11c84efddab1ffd0f266e3f70ad
Tristan <tristan.peng@stanford.edu>
2020-12-28 18:09:53 -0800

    Change filler to padding and margin

--- frontend/src/components/workstation/windows/Info.js
+++ frontend/src/components/workstation/windows/Info.js
@@ -3,7 +3,7 @@ import Window from './Window';
 const Info = ({ title, children }) => {
   return (
     <Window anchor="help-window" title={ `Info: ${ title }` }>
-      { children ? children : <div className="filler" /> }
+      { children ? children : <div className="p-20 m-20" /> }
     </Window>
   );
 };
