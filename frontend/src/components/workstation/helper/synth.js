a18122cce224aa5310c47302e8b4e4b52fad699c
Tristan <tristan.peng@stanford.edu>
2021-01-05 19:01:16 -0800

    Change default values and account for mute

--- frontend/src/components/workstation/helper/synth.js
+++ frontend/src/components/workstation/helper/synth.js
@@ -16,18 +16,18 @@ export const play = (data, globalSettings) => {
     context = new AudioContext();
 
     let bpm = globalSettings.bpm < 0 ? 60 : globalSettings.bpm;
-    let key = globalSettings.key === 'none' ? 'C' : globalSettings.key;
+    let key = globalSettings.key === 'none' ? 'chromatic' : globalSettings.key;
 
     data.forEach(d => {
       if (d.settings.channel.length === 0) {
         const osc = new OscillatorNode(context, { type: 'sine' });
         const gain = new GainNode(context);
         osc.connect(gain).connect(context.destination);
-        gain.gain.value = d.settings.volume / 100 / data.length;
+        gain.gain.value = d.settings.mute ? 0 : d.settings.volume / 100 / data.length;
 
         const max = Math.max(...d.data);
         const min = Math.min(...d.data);
-        const num = SCALES[key].length * 2; // TODO: This is incorrect
+        const num = SCALES[key].length * 2;
         const normalize = x => Math.round(num / (max - min) * (x - min));
 
         if (d.settings.continuous) {
