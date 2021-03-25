d57e83c46480f596d98267e464b9ce7aeee6aed5
Tristan <tristan.peng@stanford.edu>
2021-03-25 16:45:55 -0700

    Add connected to channel message if track is connected to channel

--- frontend/src/components/workstation/Track.js
+++ frontend/src/components/workstation/Track.js
@@ -1,13 +1,19 @@
 import { TRACK_CONTROLS_WIDTH, TRACK_WIDTH } from '../../constants/workstation';
 import Settings from './Settings';
 import TrackData from './TrackData';
+import { connect } from 'react-redux';
 
-const Track = ({ i, id, column, name }) => (
+const Track = ({ i, id, column, name, tracks }) => (
   <div className="row row-eq-spacing">
     <div className={ `col-${ TRACK_CONTROLS_WIDTH }` }>
       <div className="card track">
         <h2 className="card-title">Track { i + 1 } - { column }</h2>
         <p className="text-muted">{ name }</p>
+        { /* TODO: Fix this */ }
+        {
+          tracks.find(t => t.id === id).settings.channel.length > 0 ?
+            <p className="text-muted">Connected to channel { tracks.find(t => t.id === id).settings.channel }</p> : <></>
+        }
         <Settings column={ column } i={ i } />
       </div>
     </div>
@@ -19,4 +25,8 @@ const Track = ({ i, id, column, name }) => (
   </div>
 );
 
-export default Track;
+const mapStateToProps = state => ({
+  tracks: state.tracks
+});
+
+export default connect(mapStateToProps)(Track);
