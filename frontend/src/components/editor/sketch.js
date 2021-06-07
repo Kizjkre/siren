c08981d50a86f3238556c39b97538d36ea249ea6
Tristan <tristan.peng@stanford.edu>
2021-06-07 14:08:17 -0700

    Add buttons to add nodes

--- frontend/src/components/editor/sketch.js
+++ frontend/src/components/editor/sketch.js
@@ -1,28 +1,63 @@
 import { NodeDisplay, Oscillator, Panner, Gain } from './node';
 
-const s = (h, w) => s => {
-  let font;
-  const display = new NodeDisplay(s);
+export default class Sketch {
+  draw(h, w) {
+    this._draw = true;
+    return s => {
+      let font;
+      this._s = s;
+      this._display = new NodeDisplay(s);
 
-  s.preload = () => {
-    font = s.loadFont('https://fonts.gstatic.com/s/jetbrainsmono/v6/tDbY2o-flEEny0FZhsfKu5WU4zr3E_BX0PnT8RD8yKxTOlOTk6OThhvA.woff');
-  };
+      s.preload = () => {
+        font = s.loadFont('https://fonts.gstatic.com/s/jetbrainsmono/v6/tDbY2o-flEEny0FZhsfKu5WU4zr3E_BX0PnT8RD8yKxTOlOTk6OThhvA.woff');
+      };
 
-  s.setup = () => {
-    s.createCanvas(w, h);
-    s.textFont(font, 36);
-    display.nodes.push(new Oscillator(100, 100));
-    display.nodes.push(new Panner(200, 200));
-    display.nodes.push(new Gain(400, 400));
-    display.nodes.push(new Oscillator(100, 100));
-    s.noLoop();
-  };
+      s.setup = () => {
+        s.createCanvas(w, h);
+        s.textFont(font, 36);
+        this._display.nodes.push(new Oscillator(100, 100));
+        this._display.nodes.push(new Panner(200, 200));
+        this._display.nodes.push(new Gain(400, 400));
+        this._display.nodes.push(new Oscillator(100, 100));
+        s.noLoop();
+      };
 
-  s.draw = () => {
-    s.clear();
-    display.show();
-    console.table(Array.from(display.nodes.groups));
-  };
-};
+      s.draw = () => {
+        s.clear();
+        this._display.show();
+      };
+    };
+  }
 
-export default s;
+  add(node) {
+    if (!this._draw) return;
+    this._display.nodes.push(node);
+    this._s.redraw();
+  }
+}
+
+// const s = (h, w) => s => {
+//   let font;
+//   const display = new NodeDisplay(s);
+//
+//   s.preload = () => {
+//     font = s.loadFont('https://fonts.gstatic.com/s/jetbrainsmono/v6/tDbY2o-flEEny0FZhsfKu5WU4zr3E_BX0PnT8RD8yKxTOlOTk6OThhvA.woff');
+//   };
+//
+//   s.setup = () => {
+//     s.createCanvas(w, h);
+//     s.textFont(font, 36);
+//     display.nodes.push(new Oscillator(100, 100));
+//     display.nodes.push(new Panner(200, 200));
+//     display.nodes.push(new Gain(400, 400));
+//     display.nodes.push(new Oscillator(100, 100));
+//     s.noLoop();
+//   };
+//
+//   s.draw = () => {
+//     s.clear();
+//     display.show();
+//   };
+// };
+//
+// export default s;
