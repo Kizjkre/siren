2092535e73bb6bf7eb64ba0fc17b7277ed321b34
Tristan <24882191+Kizjkre@users.noreply.github.com>
2022-11-13 17:16:24 -0800

    Setup template

---
+++ frontend/src/index.jsx
@@ -0,0 +1,4 @@
+/* @refresh reload */
+import { render } from 'solid-js/web';
+
+render(() => <h1>Hello World!!</h1>, document.getElementById('root'));
--- frontend/src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from '@reduxjs/toolkit';
-import rootReducer from '../reducers/index';
-
-const store = createStore(rootReducer);
-
-export default store;
