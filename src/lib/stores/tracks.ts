69b517d9f00b3aeec5ff012076df1f664a43c690
Tristan <24882191+Kizjkre@users.noreply.github.com>
2023-10-30 21:23:18 -0700

    Fixed a bug where deleting a track with regions didn't update the references

--- src/lib/stores/tracks.ts
+++ src/lib/stores/tracks.ts
@@ -1,6 +1,8 @@
-import { type Writable, writable } from 'svelte/store';
+import { get, type Writable, writable } from 'svelte/store';
 import track from '$lib/stores/track';
-import type { TrackStore, TrackStoreObject } from '$lib/util/definitions/tracks';
+import type { TrackRegionStoreStructure, TrackStore, TrackStoreObject } from '$lib/util/definitions/tracks';
+import type { Region } from '$lib/util/definitions/region';
+import data from '$lib/stores/data';
 
 const store: Writable<TrackStore> = writable({});
 
@@ -8,6 +10,12 @@ const tracks: TrackStoreObject = {
   add: (): any =>
     store.update((tracks: TrackStore): TrackStore => ({ ...tracks, [new Date().getTime()]: track() })),
   remove: (id: number): any => store.update((tracks: TrackStore): TrackStore => {
+    Object.values(get(tracks[id].regions.timbral)).forEach((regions: TrackRegionStoreStructure): any =>
+      Object.values(regions).forEach((region: Region): any =>
+        get(data)[region.source.id].references.update((refs: number): number => refs - 1)
+      )
+    );
+
     delete tracks[id];
     return tracks;
   }),
